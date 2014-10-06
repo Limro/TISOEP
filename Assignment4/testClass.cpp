eb5602cd57fc9e18f0bc10b22da16cdc431ddc8e
KapSteR <hr.kasper.nielsen@gmail.com>
2014-10-06 15:33:17 +0200

    Tonser nice listing, Tag den Rasmus

---
+++ Assignment4/testClass.cpp
@@ -0,0 +1,46 @@
+[TestFixture]
+public class TestClass
+{
+	int k;
+	int m;
+	int x;
+
+	public funcion() // function to test
+	{
+		int i;
+		for(i=0; i<k; i++) {
+			m = m + x;
+		}
+	}
+
+	[Setup]
+	public void Setup() //Will set the following vaules after each test
+	{
+		m = 2;
+		x = 2;
+	}
+
+	[Test]
+	public void Loop_KEqual_minus1()
+	{
+		k = -1;
+		function();
+		assert.AreEqual(m,0) //m should be 0, but will be 2
+	}
+
+	[Test]
+	public void Loop_KEqual_0()
+	{
+		k = 0;
+		function();
+		assert.AreEqual(m,2) //Works fine
+	}
+
+	[Test]
+	public void Loop_KEqual_1()
+	{
+		k = 1;
+		function();
+		assert.AreEqual(m,4) //Works fine
+	}
+}
